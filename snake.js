bc26e3e86138ea0a77c6bb10d508e92b9a1f7278
miwanoff <marina.iva.noffa@gmail.com>
2021-01-17 16:49:42 +0200

    add class Snake

--- snake.js
+++ snake.js
@@ -55,17 +55,87 @@ class Apple {
     this.block = new Block(10, 10);
   }
 
-  draw = function () {
+  draw() {
     this.block.drawCircle("LimeGreen");
-  };
+  }
 
-  move = function () {
+  move() {
     const randomCol = Math.floor(Math.random() * (widthInBlocks - 2)) + 1;
     const randomRow = Math.floor(Math.random() * (heightInBlocks - 2)) + 1;
     this.block = new Block(randomCol, randomRow);
-  };
+  }
 }
 
+class Snake {
+  constructor() {
+    this.segments = [new Block(7, 5), new Block(6, 5), new Block(5, 5)];
+    this.direction = "right";
+    this.nextDirection = "right";
+  }
+  draw = function () {
+    for (var i = 0; i < this.segments.length; i++) {
+      this.segments[i].drawSquare("blue");
+    }
+  };
+  move = function (apple, game) {
+    let head = this.segments[0];
+    let newHead;
+    this.direction = this.nextDirection;
+
+    if (this.direction === "right") {
+      newHead = new Block(head.col + 1, head.row);
+    } else if (this.direction === "down") {
+      newHead = new Block(head.col, head.row + 1);
+    } else if (this.direction === "left") {
+      newHead = new Block(head.col - 1, head.row);
+    } else if (this.direction === "up") {
+      newHead = new Block(head.col, head.row - 1);
+    }
+
+    if (this.checkCollision(newHead)) {
+      game.gameOver();
+      return;
+    }
+
+    this.segments.unshift(newHead);
+
+    if (newHead.equal(apple.block)) {
+      game.score++;
+      apple.move();
+    } else {
+      this.segments.pop();
+    }
+  };
+
+  checkCollision = function (head) {
+    let leftCollision = head.col === 0;
+    let topCollision = head.row === 0;
+    let rightCollision = head.col === widthInBlocks - 1;
+    let bottomCollision = head.row === heightInBlocks - 1;
+    let wallCollision =
+      leftCollision || topCollision || rightCollision || bottomCollision;
+    let selfCollision = false;
+    for (let i = 0; i < this.segments.length; i++) {
+      if (head.equal(this.segments[i])) {
+        selfCollision = true;
+      }
+    }
+    return wallCollision || selfCollision;
+  };
+
+  setDirection = function (newDirection) {
+    if (this.direction === "up" && newDirection === "down") {
+      return;
+    } else if (this.direction === "right" && newDirection === "left") {
+      return;
+    } else if (this.direction === "down" && newDirection === "up") {
+      return;
+    } else if (this.direction === "left" && newDirection === "right") {
+      return;
+    }
+    this.nextDirection = newDirection;
+  };
+}
 // let sampleBlock = new Block(5, 5);
 // sampleBlock.drawCircle();
 
